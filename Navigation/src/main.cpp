bfe9c9f49921ed5a6ff8fb2b2d62f9c99d0b9010
Andrew Ning <aning97@gmail.com>
2018-01-23 22:09:11 -0500

    Added zed camera and spatial mapping parameter initialization

--- Navigation/src/main.cpp
+++ Navigation/src/main.cpp
@@ -6,6 +6,33 @@
 #include<opencv2/opencv.hpp>
 #include<Gl>
 
-int main{
+int main(){
 
+    //Zed objects
+    Camera zed;         //Camera object
+    sl::Pose pose;      // Pose to hold positional tracking data
+    sl::Mesh mesh;      // Mesh to hold the mesh generated during spatial mapping
+
+    // Set configuration parameters
+    InitParameters init_params;
+    init_params.camera_resolution = RESOLUTION_HD1080;
+    init_params.depth_mode = DEPTH_MODE_PERFORMANCE;
+    init_params.coordinate_units = UNIT_METER;
+    init_params.camera_fps = 100;
+
+    // Configure Spatial Mapping and filtering parameters
+    sl::SpatialMappingParameters spatial_mapping_params;
+    spatial_mapping_params.range_meter = sl::SpatialMappingParameters::get(sl::SpatialMappingParameters::MAPPING_RANGE_FAR); //MAPPING_RANGE_FAR specified for outdoor use
+    spatial_mapping_params.resolution_meter = sl::SpatialMappingParameters::get(sl::SpatialMappingParameters::MAPPING_RESOLUTION_LOW); //Low resolution to detect only large variations to save on memory
+    spatial_mapping_params.save_texture = false;        //Textures not needed
+    spatial_mapping_params.max_memory_usage = 512;
+    spatial_mapping_params.use_chunk_only = true; // If we use chunks we do not need to keep the mesh consistent
+
+    // Open the camera
+    ERROR_CODE err = zed.open(init_params);
+    if (err != SUCCESS) {
+        printf("%s\n", errorCode2str(err).c_str());
+        zed.close();
+        return 1; // Quit if an error occurred
+    }
 }
