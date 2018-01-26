45bc86f73fe202b109c317aba200a5f75d540e5f
djorna <davidjorna@gmail.com>
2018-01-25 20:18:01 -0500

    finished roughness function

--- Navigation/src/getRoughness.cpp
+++ Navigation/src/getRoughness.cpp
@@ -2,44 +2,43 @@
 #include <cmath>
 #includ <algorithm>
 
-float avAngle(sl::Chunk chunk, float3 vertex, int v){
+float avAngle(sl::Chunk chunk, sl::float3 vertex, int v){
 	size_t N_tri = chunk.triangles.size();
-	
-	//keep track of which edges have been checked
-	//size_t N_vert = chunk.vertices.size();
-	//create N_vert x N_vert matrix to keep track of visited cells
-	//std::vector<vector<bool>> visited(N_vert,vector<bool>(N_vert));
+	vector<int> neighbours;
+	float avAngle = 0;
+	std::vector<int>::iterator it;
 	
 	//get v of all neighbour vertices
-	vector<int> neighbours;
-	std::vector<int2> edges;
-	float avAngle = 0;
 	for(size_t i=0;i<N_tri;i++){
 		//if triangle contains vertex, push other vertices
-		vector<float3> tri = chunk.triangles[i];
+		std::vector<sl::float3> tri = chunk.triangles[i];
+		p = std::find (myints, myints+4, 30);
 		if(std::find(tri.begin(), tri.end(), v) != tri.end()){
 			for(int j=0;j<2;j++){
 				if(tri[j]!=v){
 					neighbours.push_back(tri[j]);
 				}
 			}
-		}		
+		}
 	}
 	
 	//calculate average angle of surrounding normals
-	for(int u=0;u<tri.size();u++){
-		avAngle += acos(chunk.normal[u]*chunk.normal[v]));
+	int Nd = neighbours.size(); //direct neighbours of v
+	for(int u=0;u<Nd;u++){
+		avAngle += acos(sl::float3.dot(chunk.normals[u],chunk.normals[v]));
 	}
 	avAngle /= Nd;
 }
 
-float getRoughness(sl::Chunk chunk){
-	//std::vector<sl::float3> vertices
-	float roughness;
-	int N = chunk.size();
+float getRoughness(sl::Chunk chunk,sl::){
+	float roughness = avAngle();
+	int Nl = chunk.size();
 	
-	for(size_t i=0;i<N;i++){
-		
+	//get angle of all points in chunk
+	for(size_t i=0;i<Nl;i++){
+		roughness += avAngle(chunk,chunk.vertices[i],i);
 	}
 	
+	//normalize
+	roughness /= Nl;	
 }
